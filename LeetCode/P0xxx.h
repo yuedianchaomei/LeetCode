31e476d8c683c2cef68b80c494dc87189b8230ab
1242438157 <1242438157@qq.com>
2025-05-31 18:30:20 +0800

    每日一题

---
+++ LeetCode/P0xxx.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "Solution.h"
+#include "DataParser.h"
+
+class P0909 {
+private:
+    int snakesAndLadders(vector<vector<int>>& board) {
+        int n = board.size();
+        vector<int> boards(n * n + 1);
+        bool dir = true;
+        for (int i = n - 1, k = 1; i >= 0; --i) {
+            if (dir) {
+                for (int j = 0; j < n; ++j, ++k) {
+                    boards[k] = board[i][j];
+                }
+                dir = false;
+            }
+            else {
+                for (int j = n - 1; j >= 0; --j, ++k) {
+                    boards[k] = board[i][j];
+                }
+                dir = true;
+            }
+        }
+        vector<bool> visit(n * n + 1, false);
+        queue<int> que;
+        que.push(1);
+        visit[1] = true;
+        int times = 0;
+        while (!que.empty()) {
+            int queSize = que.size();
+            for (int i = 0; i < queSize; ++i) {
+                int cur = que.front();
+                que.pop();
+                if (cur >= n * n) {
+                    return times;
+                }
+                for (int step = 1; step <= 6; ++step) {
+                    int nxt = cur + step;
+                    nxt = min(nxt, n * n);
+                    if ((boards[nxt] != -1) && !visit[boards[nxt]]) {
+                        visit[boards[nxt]] = true;
+                        que.push(boards[nxt]);
+                    }
+                    else if (!visit[nxt] && boards[nxt] == -1) {
+                        visit[nxt] = true;
+                        que.push(nxt);
+                    }
+                }
+            }
+            ++times;
+        }
+        return -1;
+    }
+
+public:
+    void run() {
+        vector<vector<int>> board = DataParser::parseInt2DArray("[[-1,-1,-1,46,47,-1,-1,-1],[51,-1,-1,63,-1,31,21,-1],[-1,-1,26,-1,-1,38,-1,-1],[-1,-1,11,-1,14,23,56,57],[11,-1,-1,-1,49,36,-1,48],[-1,-1,-1,33,56,-1,57,21],[-1,-1,-1,-1,-1,-1,2,-1],[-1,-1,-1,8,3,-1,6,56]]");
+        cout << snakesAndLadders(board);
+    }
+};
--- LeetCode/run.cpp
+++ LeetCode/run.cpp
@@ -1,9 +1,10 @@
+#include "P0xxx.h"
 #include "P2xxx.h"
 #include "P3xxx.h"
 
 int main() {
 
-	P2359 problem;
+	P0909 problem;
 
 	problem.run();
 
